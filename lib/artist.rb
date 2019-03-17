d2b6e124ff0ad3e414bd89e3442178f72a0c4c2d
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-17 18:16:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ class Artist
  attr_accessor :name, :songs
 
  @@all = []
- 
+ require "pry"
  def initialize (name)
     @name = name
     @@all << self
@@ -19,6 +19,7 @@ class Artist
 
 def new_song (name, genre)
   song = Song.new(name,self,genre)
+  binding.pry
 end 
   
   
