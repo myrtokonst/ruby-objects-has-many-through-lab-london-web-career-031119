791ddbe9ff95affc66cb85e26b1fd59be7512805
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-17 18:14:14 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist 
+ attr_accessor :name, :songs
+
+ @@all = []
+ 
+ def initialize (name)
+    @name = name
+    @@all << self
+    @songs = []
+ end 
+ 
+ def self.all 
+   @@all
+ end
+ 
+ def songs 
+   @songs 
+ end 
+
+def new_song (name, genre)
+  song = Song.new(name,self,genre)
+   @songs << song
+end 
+  
+  
+end 
